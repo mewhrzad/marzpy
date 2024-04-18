e69638189187f0a3815bd3e57471d7f396d15230
Mehrzad Khosravi <77574556+mewhrzad@users.noreply.github.com>
2024-04-18 18:20:03 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="marzpy",
-    version="0.0.4",
+    version="0.0.5",
     author="Mewhrzad",
     description="a simple application with python to manage Marzban panel",
     long_description="text/markdown",
